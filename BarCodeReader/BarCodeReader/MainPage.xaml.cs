87221dc40a62a1e789c72e30d65a948f93b35b5a
egbakou <egbakoukodjolaurent@gmail.com>
2019-02-19 10:17:16 +0000

    Project created and nuget packages added

---
+++ BarCodeReader/BarCodeReader/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BarCodeReader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
